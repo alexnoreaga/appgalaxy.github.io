82e94145a9a9c85b46ead5401ecdc2c684e3642d
alexnoreaga <35931297+alexnoreaga@users.noreply.github.com>
2019-12-27 20:43:24 +0700

    update

---
+++ paket/script.js
@@ -0,0 +1,121 @@
+function paketA() {
+    var x = document.getElementById("paketa");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function paketB() {
+    var x = document.getElementById("paketb");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function paketB2() {
+    var x = document.getElementById("paketb2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+
+function paketC() {
+    var x = document.getElementById("paketc");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function paketC2() {
+    var x = document.getElementById("paketc2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function paketD() {
+    var x = document.getElementById("paketd");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function paketD2() {
+    var x = document.getElementById("paketd2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function paketE() {
+    var x = document.getElementById("pakete");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function paketE2() {
+    var x = document.getElementById("pakete2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function paketF() {
+    var x = document.getElementById("paketf");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function paketF2() {
+    var x = document.getElementById("paketf2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function paketG() {
+    var x = document.getElementById("paketg");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+function paketG2() {
+    var x = document.getElementById("paketg2");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+
+  function myReload() {
+    location.reload();
+  }
